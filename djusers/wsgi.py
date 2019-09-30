df33474142c666bd0cde208440b3849c1979a514
Ripudaman Singh <rds0751@gmail.com>
2019-09-30 17:51:51 +0530

    bshb

--- djusers/wsgi.py
+++ djusers/wsgi.py
@@ -9,9 +9,27 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+import time
+import traceback
+import signal
+import sys
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djusers.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
+
+
+# application = get_wsgi_application()
 
