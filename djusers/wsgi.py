cfcdc9ba14dd50cd898e81fcfa9514e26546672f
Ripudaman Singh <rds0751@gmail.com>
2019-09-30 12:51:00 +0530

    msg

--- djusers/wsgi.py
+++ djusers/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djusers.settings')
 
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '': 
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
+
 application = get_wsgi_application()
+
